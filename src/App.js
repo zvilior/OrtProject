fe8c245003ad2c74c8017be6bebe51b14457c877
zvilior <102918963+zvilior@users.noreply.github.com>
2022-11-22 12:34:40 +0200

    sume changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import ProductsList from './ProductsList';
 
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-    <ProductsList/>
+        <ProductsList />
 
 
       </header>
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 let props = {
     name: "sadas",
     price: 328
